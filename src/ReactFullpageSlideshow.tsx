716fe4b89712251cb05a997d15c62d67f8c414aa
Toby Liu <ybot1122@gmail.com>
2024-10-19 20:02:36 -0700

    Slide Container

--- src/ReactFullpageSlideshow.tsx
+++ src/ReactFullpageSlideshow.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export default function ReactFullpageSlideshow({label}: {label: string}) {
-  return <p>{label}</p>
+export default function ReactFullpageSlideshow({items}: {items: JSX.Element[]}) {
+
+  const itemsWrapped = items.map((item, ind) => <SlideContainer key={ind + '-fullpage-slideshow'}>{item}</SlideContainer>)
+
+  return <div style={{
+    overflow: 'hidden',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: '100vh',
+    width: '100vw'
+  }}>{itemsWrapped}</div>
+}
+
+const SlideContainer = ({children}: {children: JSX.Element}) => {
+
+  return (<section style={{
+    position: 'relative',
+    width: '100%',
+    height: '100%'
+  }}>
+
+
+    {children}
+
+  </section>)
+
 }
