2144bed17e48d8bd1765341cb05905fb445b9f68
Toby Liu <ybot1122@gmail.com>
2024-10-21 19:09:53 -0700

    Adds Eslint. Creates Initial goToSlide API. (#2)
    This pull request adds ESlint to the developer workflow.
    
    It adds a `goToSlide` method which is supposed to be passed into
    SlideShow items.
    
    This interface will be improved in future versions.

--- src/ReactFullpageSlideshow.tsx
+++ src/ReactFullpageSlideshow.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 
-export default function ReactFullpageSlideshow({label}: {label: string}) {
-  return <p>{label}</p>
+export default function ReactFullpageSlideshow({items}: {items: ReactFullpageSlideshowItem[]}) {
+
+  const activeIndexRef = useRef(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const goToSlide = useCallback((index: number) => {
+    if (index >= 0 && index < items.length) {
+      setActiveIndex(index)
+      activeIndexRef.current = index;
+    }
+  }, [setActiveIndex]);
+
+  const itemsWrapped = items.map((item, ind) => <SlideContainer goToSlide={goToSlide} index={ind} activeIndex={activeIndex} key={ind + '-fullpage-slideshow'}>{item}</SlideContainer>)
+
+  return <div style={{
+    overflow: 'hidden',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: '100vh',
+    width: '100vw'
+  }}>{itemsWrapped}</div>
+}
+
+type GoToSlide = (index: number) => void;
+type ReactFullpageSlideshowItem = (goToSlide: GoToSlide) => JSX.Element;
+
+const SlideContainer = ({children, index, activeIndex, goToSlide}: {children: ReactFullpageSlideshowItem, index: number, activeIndex: number, goToSlide: GoToSlide}) => {
+  let top = "0px";
+  if (index < activeIndex) {
+    top = "-100vh";
+  } else if (index > activeIndex) {
+    top = "100vh";
+  }
+
+  return (<section style={{
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    zIndex: index * 100,
+    top,
+  }}>
+
+
+    {children(goToSlide)}
+
+  </section>)
+
 }
